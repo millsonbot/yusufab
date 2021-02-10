a5d741322f9d56bfd84b3833c4e3292c1fbe9243
millsonbot <77483817+millsonbot@users.noreply.github.com>
2021-02-10 12:55:39 +0300

    Update lordcreative.js

--- data/lordcreative.js
+++ data/lordcreative.js
@@ -15,17 +15,7 @@ function sleep(milliseconds) {
 const http = require("http");
 app.get("", (request, response) => {
   console.log(Date.now() + " Ping tamamdır.");
-  response.sendStatus(200);
-});
-
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const data = new Map();
-
-
-
-const lordcreative1 = new Discord.Client("ODA4ODEyNTY0MzMwMTg0NzE0.YCL_ig.NgOPtwMBpIM8au0_jyuiB94SePw");
+  response.sendStatus(2");
 const lordcreative2 = new Discord.Client();
 const lordcreative3 = new Discord.Client();
 const lordcreative4 = new Discord.Client();
@@ -46,34 +36,6 @@ const lordcreative18 = new Discord.Client();
 const lordcreative19 = new Discord.Client();
 const lordcreative20 = new Discord.Client();
 const lordcreative21 = new Discord.Client();
-const lordcreative22 = new Discord.Client();
-const lordcreative23 = new Discord.Client();
-const lordcreative24 = new Discord.Client();
-
-lordcreative1.login("Nzk4NDYwMTI2OTI2MzQwMDk2.X_1V8g.77K23-HETchniiDyXfGgjmw4OGo");
-lordcreative2.login("Nzk4NDU4ODQ1MzUyOTUxODA4.X_1Uzg.mrE_7huxQMblwAZnXK1Dn9WVPaA");
-lordcreative3.login("Nzk4NDU5NDUzODY3MTYzNjk4.X_1VUg.dnqM2XB8AJ4flqNKIldGVMRqp6I");
-lordcreative4.login("Nzk4NDU5NzI3MTMzNTQwMzYz.X_1Vkw.sNB-XFZCC_AbDVeGIMh_R7pBnoA");
-lordcreative5.login("Nzk4NDYwNDkyNjYxNzg0NTc2.X_1WSQ.C_XbtoXf9ytgV9G3D35BSeNIAbg");
-lordcreative6.login("Nzk4NDYxMTA0NzM2MzA1MTYz.X_1W2w.OG4R4IkxYNmmJVFTzXP8hkH68Wc");
-lordcreative7.login("Nzk4NDYxNDIxNDQwMjA0ODQw.X_1XJw.biv1CVc-Mu5s6NKz-aOQ-i-DKA0");
-lordcreative8.login("Nzk4NDYyNzUyMzIyMDkzMDY3.X_1YZA.kWl_jSBW3WcFG9fACPETiFxf4NA");
-lordcreative9.login("Nzk4NDYzMjMwNzQ0OTg1NjA0.X_1Y1g.9UToMfvbfETrE8z2gDiuH4jzB4c");
-lordcreative10.login("Nzk4NDYzODMwODc5MjQwMjEy.X_1ZZQ.fi3vOw9jEa5saA54LUjhGRDb_A4");
-lordcreative11.login("Nzk4NDY0MjYyNzQ5NTUyNjgw.X_1ZzA.xgoBTtXFR3VxjYaIpWKUChe_hqY");
-lordcreative12.login("Nzk4NDY0NDQzNTczODYyNDEx.X_1Z9w.YhUMhH2iX7nMgxAAfoXSXN5UB7Y");
-lordcreative13.login("Nzk4NDY0NzMzNzE1MTAzNzQ0.X_1aPA.WIWVRPeC3_KxsAlB8S4CNmOdbd4");
-lordcreative14.login("Nzk4NDY0OTk5NzYwNDYxODM0.X_1afQ.SzLQ-VYpcGaZ1i3w-0OiIR8_tw8");
-lordcreative15.login("Nzk4NDY1MzU5NDUwODY1Njc1.X_1a0g.3GDEbydMaccAsTDx35d2wjszKGY");
-lordcreative16.login("Nzk4NDY3NTQyNTExMjU1NTcz.X_1c2g.aurmZ_-99NtqrK-e8cHW9HKWRmQ");
-lordcreative17.login("")
-lordcreative18.login("")
-lordcreative19.login("")  
-lordcreative20.login("")
-lordcreative21.login("")
-lordcreative22.login("")
-lordcreative23.login("")
-lordcreative24.login("")
 
 
 
